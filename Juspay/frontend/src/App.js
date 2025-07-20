c1fd1463f1708ce603f5f69fcc967044d2aa97dd
Hydenx2004 <hyden741@gmail.com>
2025-07-20 16:44:50 +0530

    test

--- Juspay/frontend/src/App.js
+++ Juspay/frontend/src/App.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './App.css';
 
-const API_BASE_URL = 'https://13.232.57.109:5000';
+const API_BASE_URL = 'https://localhost:5000';
 
 function App() {
   const [treeData, setTreeData] = useState({});
